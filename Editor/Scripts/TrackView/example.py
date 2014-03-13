44238fc4942ee737232556ed7e35f887a061d90d
Jason.howard <jasonh78@gmail.com>
2014-03-13 09:50:07 -0700

    removing files which are not needed in the repo
    The bin dirs and editor dirs will be created with the archive from
    crydev.net. that should be added to the development git dir once created
    after a clone. all folders should be added other then code. * DO NOT ADD
    CODE DIR *

--- Editor/Scripts/TrackView/example.py
+++
@@ -1,8 +0,0 @@
-'''
-This example script prints info of all TrackView sequences
-'''
-
-numTracks = trackview.get_num_sequences()
-
-for i in range(numTracks):
-	print "Sequence '", trackview.get_sequence_name(i), "':"
